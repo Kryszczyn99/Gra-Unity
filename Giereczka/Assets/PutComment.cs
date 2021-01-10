216ebfb60f51298507b1901f927af933f07d959b
Kryszczyn99 <krzysiopijanowski@gmail.com>
2021-01-10 20:29:34 +0100

    Gra-dodanie-tekstu
    Dodanie tekstów do gry, ułatwiających rozgrywkę oraz parę updateów (prędkości i naprawa bugów).

---
+++ Giereczka/Assets/PutComment.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PutComment : MonoBehaviour
+{
+    public GameObject Text;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Text.SetActive(false);
+    }
+
+    void OnTriggerEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Text.SetActive(true);
+        }
+    }
+    void OnTriggerExit2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Text.SetActive(false);
+        }
+    }
+    void OnCollisionEnter2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Text.SetActive(true);
+        }
+    }
+    void OnCollisionExit2D(Collider2D player)
+    {
+        if (player.gameObject.tag == "Player")
+        {
+            Text.SetActive(false);
+        }
+    }
+}
